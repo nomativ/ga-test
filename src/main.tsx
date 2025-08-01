b7b80d7c36f3b3220fa203fcdab0055a80f12278
nomativ <toedgar@pm.me>
2025-08-01 11:14:32 -0700

    fix vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Analytics } from "@vercel/analytics/react"
-import { MixpanelClient } from './lib/mixpanelClient';
+import { MixpanelClient } from './lib/MixpanelClient.tsx'
 import './index.css'
 import App from './App.tsx'
 
